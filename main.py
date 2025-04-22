c22f3ee3887acc33f3f6645d704b0fc5b3edee05
Mario Linares <mariolinares109@gmail.com>
2025-04-22 08:39:27 -0700

    Send data form form to backend and perform counterfacetuals and loan status

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 """"""
 
+import joblib
+import warnings
+import pandas as pd
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sklearn.preprocessing import StandardScaler
+from lime.lime_tabular import LimeTabularExplainer
+from dice_ml import Model
+from dice_ml import Dice
+from dice_ml import Data
+from dice_ml.utils import helpers
+warnings.filterwarnings("ignore")
 
 app = FastAPI()
 
@@ -40,28 +50,77 @@ async def result(
     JobTenure: int = Form(...),
     NetWorth: int = Form(...)
 ):
-    input_data = {
-        "Age": Age,
-        "AnnualIncome": AnnualIncome,
-        "CreditScore": CreditScore,
-        "EmploymentStatus": EmploymentStatus,
-        "EducationLevel": EducationLevel,
-        "Experience": Experience,
-        "LoanAmount": LoanAmount,
-        "LoanDuration": LoanDuration,
-        "NumberOfDependents": NumberOfDependents,
-        "MonthlyDebtPayments": MonthlyDebtPayments,
-        "NumberOfOpenCreditLines": NumberOfOpenCreditLines,
-        "PaymentHistory": PaymentHistory,
-        "LengthOfCreditHistory": LengthOfCreditHistory,
-        "SavingsAccountBalance": SavingsAccountBalance,
-        "CheckingAccountBalance": CheckingAccountBalance,
-        "TotalAssets": TotalAssets,
-        "TotalLiabilities": TotalLiabilities,
-        "JobTenure": JobTenure,
-        "NetWorth": NetWorth,
-    }
-    return templates.TemplateResponse("result.html", {
-        "request": request,
-        "input_data": input_data,
+    # Load MLP model
+    mlp = joblib.load('model/loan_approval_model.pkl')
+
+    # Load scaler
+    scaler = joblib.load('model/scaled.pkl')
+
+    # Load data 
+    X = joblib.load('model/data.pkl')
+
+    # Load y
+    y = joblib.load('model/labels.pkl')
+
+    # Load training data
+    X_train = joblib.load('model/train_data.pkl')
+
+    input_data = pd.DataFrame({
+        'Age': [Age],
+        'AnnualIncome': [AnnualIncome],
+        'CreditScore': [CreditScore],
+        'EmploymentStatus': [EmploymentStatus],
+        'EducationLevel': [EducationLevel],
+        'Experience': [Experience],
+        'LoanAmount': [LoanAmount],
+        'LoanDuration': [LoanDuration],
+        'NumberOfDependents': [NumberOfDependents],
+        'MonthlyDebtPayments': [MonthlyDebtPayments],
+        'NumberOfOpenCreditLines': [NumberOfOpenCreditLines],
+        'PaymentHistory': [PaymentHistory],
+        'LengthOfCreditHistory': [LengthOfCreditHistory],
+        'SavingsAccountBalance': [SavingsAccountBalance],
+        'CheckingAccountBalance': [CheckingAccountBalance],
+        'TotalAssets': [TotalAssets],
+        'TotalLiabilities': [TotalLiabilities],
+        'JobTenure': [JobTenure],
+        'NetWorth': [NetWorth]
     })
+
+    # Apply the same StandardScaler used during training
+    input_data_sacled = scaler.transform(input_data)
+    
+    # Return Status of application
+    loan_status = mlp.predict(input_data_sacled)
+    print(f"Loan Status: {loan_status}")
+
+    # Create Explianer object
+    explainer = LimeTabularExplainer(
+        training_data=X_train,
+        training_labels=y,
+        feature_names=X.columns,
+        class_names=['Not Approved', 'Approved'],
+        discretize_continuous=True
+    )
+
+    # Explain a local prediction
+    row = input_data_sacled[0].reshape(1, -1)
+    explain = explainer.explain_instance(row[0], mlp.predict_proba, num_features=5)
+
+    # Transorm dataset to DiCE Data
+    X['LoanApproval'] = y
+    continuous_features = ['Age', 'AnnualIncome', 'CreditScore', 'LoanAmount', 'LoanDuration', 
+                        'MonthlyDebtPayments', 'NumberOfOpenCreditLines', 'PaymentHistory', 
+                        'LengthOfCreditHistory', 'SavingsAccountBalance', 'CheckingAccountBalance', 
+                        'TotalAssets', 'TotalLiabilities', 'JobTenure', 'NetWorth']
+    dice_data = Data(dataframe=X, continuous_features=continuous_features, outcome_name='LoanApproval')
+
+    # Create DiCE model
+    dice_model = Model(model=mlp, backend='sklearn')
+
+    # Create dice explainer
+    dice_explain = Dice(dice_data, dice_model, method='random')
+
+    dice_counterfactual = dice_explain.generate_counterfactuals(input_data, total_CFs=5, desired_class='opposite', verbose=False)
+
+    return templates.TemplateResponse("result.html", {"request": request})
